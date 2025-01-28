521e65530439fdb17af73cb845adc8b0e84b29f8
Biscu1t1 <alonmalka360@gmail.com>
2025-01-28 15:35:09 +0200

    setup

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import pygame
 if __name__ == "__main__":
     pygame.init()
 
-    
     screen = pygame.display.set_mode((800, 600))
     pygame.display.set_caption("Snake Game")
     running = True
