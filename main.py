abbdd584a122199de13046cb4e2dc9906c22bc93
Biscu1t1 <alonmalka360@gmail.com>
2025-01-28 15:31:13 +0200

    initialize pygame and create a basic window for the Snake Game

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame
+
+if __name__ == "__main__":
+    pygame.init()
+
+    
+    screen = pygame.display.set_mode((800, 600))
+    pygame.display.set_caption("Snake Game")
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+    pygame.quit()
